ffc27fa44c4ceaa9b8a7db4ee687f0ba570a2326
yangdu0731 <yangdu0731@163.com>
2021-08-20 10:41:38 +0800

    update

--- server/02_build.sh
+++ server/02_build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Usage: bash 02_build.sh
 
+if [[ -f 00_shadowsocks_all.sh ]];then
+  chmod +x 00_shadowsocks_all.sh && ./00_shadowsocks_all.sh 2>&1 | tee 00_shadowsocks_all.log
+else  
+  wget --no-check-certificate -O 00_shadowsocks_all.sh https://raw.githubusercontent.com/yangdu0731/dlcore/base/server/00_shadowsocks_all.sh && chmod +x 00_shadowsocks_all.sh && ./00_shadowsocks_all.sh 2>&1 | tee 00_shadowsocks_all.log
+fi
 
-wget --no-check-certificate -O 00_shadowsocks_all.sh https://raw.githubusercontent.com/yangdu0731/dlcore/base/server/00_shadowsocks_all.sh && chmod +x 00_shadowsocks_all.sh && ./00_shadowsocks_all.sh 2>&1 | tee 00_shadowsocks_all.log
-wget --no-check-certificate https://raw.githubusercontent.com/yangdu0731/dlcore/base/server/01_bbr.sh && chmod +x 01_bbr.sh && ./01_bbr.sh
+if [[ -f 01_bbr.sh ]];then
+  chmod +x 01_bbr.sh && ./01_bbr.sh
+else
+  wget --no-check-certificate https://raw.githubusercontent.com/yangdu0731/dlcore/base/server/01_bbr.sh && chmod +x 01_bbr.sh && ./01_bbr.sh
+fi
